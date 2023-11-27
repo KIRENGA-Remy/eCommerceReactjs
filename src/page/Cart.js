d79829fcc7580cb2ac9b7cf3e408289cb33ea81b
Remy_Claudien <gitoliremy.globalsimplilearn.net>
2023-11-27 23:08:50 +0200

    Final commit

--- src/page/Cart.js
+++ src/page/Cart.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import CartProduct from "../component/cartProduct";
-import emptyCartImage from "../assest/empty.gif"
+import emptyCartImage from "../assest/empty.gif";
 import { toast } from "react-hot-toast";
-import {loadStripe} from '@stripe/stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import { useNavigate } from "react-router-dom";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
@@ -11,9 +11,9 @@ const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 const Cart = () => {
   const productCartItem = useSelector((state) => state.product.cartItem);
   console.log(productCartItem);
-  const user = useSelector(state => state.user)
+  const user = useSelector(state => state.user);
   console.log(user);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const totalPrice = productCartItem.reduce(
     (acc, curr) => acc + parseInt(curr.total),
@@ -25,100 +25,115 @@ const Cart = () => {
   );
 
   const handlePayment = async () => {
-    if (user.email) {
-        const stripe = await stripePromise;
+    
+    console.log('called ')
 
-        // Create an array called 'items' containing the necessary data from 'productCartItem'
-        const items = productCartItem.map((item) => ({
-            id: item._id,
-            name: item.name,
-            price: item.price,
-            quantity: item.qty,
-        }));
-        const fetchData = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/checkout-payment`, {
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify({ items }), // Send the 'items' array to the server
+    if (user.email) {
+      console.log("user.email exists")
+      const stripe = await stripePromise;
+
+      const items = productCartItem.map((item) => {
+        return {
+          id: item._id,
+          name: item.name,
+          price: item.price,
+          quantity: item.qty,  
+        }
+      }
+    );
+      
+      console.log("items",items);
+
+      try {
+
+        console.log("backend url ",process.env.REACT_APP_SERVER_DOMIN)
+
+        const fetchData = await fetch(`http://localhost:8080/checkout-payment`, {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({ items }),
         });
 
-        if (!fetchData.ok) {
-            console.error("Error in fetching data:", fetchData.statusText);
-            return;
-        }
+        console.log("data from backend",fetchData);
 
-        const session = await fetchData.json();
+        // if (!fetchData.ok) {
+        //   console.error("Error in fetching data:", fetchData.statusText);
+        //   return;
+        // }
+
+        const data = await fetchData.json();
+
         toast("Redirect to the payment Gateway...!");
         const result = await stripe.redirectToCheckout({
-            sessionId: session.id,
+          sessionId: data.sessionId,
         });
-    } else {
-        setTimeout(() => {
-            toast("You didn't log in!");
-            navigate("/login");
-        }, 2000);
-    }
-};
 
+      } catch (error) {
+        console.error('Error during payment:', error);
+      }
+    }
+  };
 
   return (
     <>
-    
       <div className="p-2 md:p-4">
         <h2 className="text-lg md:text-2xl font-bold text-slate-600">
           Your Cart Items
         </h2>
 
         {productCartItem[0] ?
-        <div className="my-4 flex gap-3">
-          {/* display cart items  */}
-          <div className="w-full max-w-3xl ">
-            {productCartItem.map((el) => {
-              return (
-                <CartProduct
-                  key={el._id}
-                  id={el._id}
-                  name={el.name}
-                  image={el.image}
-                  category={el.category}
-                  qty={el.qty}
-                  total={el.total}
-                  price={el.price}
-                />
-              );
-            })}
+          <div className="my-4 flex gap-3">
+            {/* display cart items  */}
+            <div className="w-full max-w-3xl ">
+              {productCartItem.map((el) => {
+                return (
+                  <CartProduct
+                    key={el._id}
+                    id={el._id}
+                    name={el.name}
+                    image={el.image}
+                    category={el.category}
+                    qty={el.qty}
+                    total={el.total}
+                    price={el.price}
+                  />
+                );
+              })}
+            </div>
+
+            {/* total cart item  */}
+            <div className="w-full max-w-md  ml-auto">
+              <h2 className="bg-blue-500 text-white p-2 text-lg">Summary</h2>
+              <div className="flex w-full py-2 text-lg border-b">
+                <p>Total Qty :</p>
+                <p className="ml-auto w-32 font-bold">{totalQty}</p>
+              </div>
+              <div className="flex w-full py-2 text-lg border-b">
+                <p>Total Price</p>
+                <p className="ml-auto w-32 font-bold">
+                  <span className="text-red-500">₹</span> {totalPrice}
+                </p>
+              </div>
+              <button
+               className="bg-red-500 w-full text-lg font-bold py-2 text-white" onClick={()=>{
+                handlePayment()
+               }}>
+                Payment
+              </button>
+            </div>
           </div>
 
-          {/* total cart item  */}
-          <div className="w-full max-w-md  ml-auto">
-            <h2 className="bg-blue-500 text-white p-2 text-lg">Summary</h2>
-            <div className="flex w-full py-2 text-lg border-b">
-              <p>Total Qty :</p>
-              <p className="ml-auto w-32 font-bold">{totalQty}</p>
+          :
+          <>
+            <div className="flex w-full justify-center items-center flex-col">
+              <img src={emptyCartImage} className="w-full max-w-sm" alt="Empty Cart" />
+              <p className="text-slate-500 text-3xl font-bold">Empty Cart</p>
             </div>
-            <div className="flex w-full py-2 text-lg border-b">
-              <p>Total Price</p>
-              <p className="ml-auto w-32 font-bold">
-                <span className="text-red-500">₹</span> {totalPrice}
-              </p>
-            </div>
-            <button className="bg-red-500 w-full text-lg font-bold py-2 text-white" onClick={handlePayment}>
-              Payment
-            </button>
-          </div>
-        </div>
-
-        : 
-        <>
-          <div className="flex w-full justify-center items-center flex-col">
-            <img src={emptyCartImage} className="w-full max-w-sm"/>
-            <p className="text-slate-500 text-3xl font-bold">Empty Cart</p>
-          </div>
-        </>
-      }
+          </>
+        }
       </div>
-    
     </>
   );
 };
